a954bb5f89b945920fdbde35d517ee0dff8a49e5
kate <vuk_kat@mail.ru>
2022-05-17 11:29:31 +0500

    fix comments

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -7,11 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,7 +19,7 @@ class CallbackTest {
     //метод, в котором прописан путь до драйвера
     @BeforeAll      //будет запускаться перед всеми тестами
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");     //"название драйвера", "путь до драйвера из папки driver"
+        //System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");     //"название драйвера", "путь до драйвера из папки driver"
         WebDriverManager.chromedriver().setup();
     }
 
@@ -33,6 +30,7 @@ class CallbackTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");        //открывает браузер по URL
     }
 
     //метод закрытия всех окон браузера
@@ -44,26 +42,20 @@ class CallbackTest {
 
     @Test
     public void shouldTestCorrectData() {       //корректные данные
-        driver.get("http://localhost:9999");        //открывает браузер по URL
-        List<WebElement> elements = driver.findElements(By.className("input__control"));        //найти все элементы с атрибутом класса "input__control"
-        elements.get(0).sendKeys("Иванов Иван");     //передать значение первому элементу списка, т.е. к полю для имени
-        elements.get(1).sendKeys("+79194885321");        //передать значение второму элементу списка, т.е. к полю для телефона
-
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов-Петров Иван");       //передать значение полю для имени
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79194885321");       //передать значение полю для телефона
         driver.findElement(By.className("checkbox__box")).click();        //нажатие на чекбокс, обращаемся по атрибуту "checkbox__box"
         driver.findElement(By.tagName("button")).click();        //нажатие на кнопку Отправить, обращаемся по имени тега
         //создание переменной, куда положим текст элемента (открывшегося окна), поиск по части атрибута "alert-success"
-        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id]")).getText();
 
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());     //сравнение, проверка фактического и ожидаемого результата
     }
 
     @Test
     public void shouldTestInvalidFieldName() {     //проверка поля имя
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Ivan Ivanov");
-        elements.get(1).sendKeys("+79194885321");
-
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ivan Ivanov-Petrov");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79194885321");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
 
@@ -74,11 +66,8 @@ class CallbackTest {
 
     @Test
     public void shouldTestEmptyFieldName() {     //проверка пустого поля имя
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("");
-        elements.get(1).sendKeys("+79194885321");
-
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79194885321");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
 
@@ -89,11 +78,8 @@ class CallbackTest {
 
     @Test
     public void shouldTestInvalidPhoneField() {     //проверка поля телефон
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иванов Иван");
-        elements.get(1).sendKeys("+7919488532");
-
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов-Петров Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+7919488532");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
 
@@ -104,11 +90,8 @@ class CallbackTest {
 
     @Test
     public void shouldTestEmptyPhoneField() {     //проверка пустого поля телефон
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иванов Иван");
-        elements.get(1).sendKeys("");
-
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов-Петров Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
 
@@ -119,12 +102,8 @@ class CallbackTest {
 
     @Test
     public void shouldTestCheckboxIsNotClicked() {     //проверка не нажатого чекбокса
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иванов Иван");
-        elements.get(1).sendKeys("+79194885321");
-
-        //driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов-Петров Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79194885321");
         driver.findElement(By.tagName("button")).click();
 
         String text = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid .checkbox__text")).getText();
